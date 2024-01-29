faee3611b27ca84039d49149dc5f27c56aad162f
Noortaz Ahmed <60826787+nahmxp@users.noreply.github.com>
2024-01-29 16:27:56 +0600

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -13,25 +13,43 @@ class MyApp extends StatelessWidget{
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.cyan,
+        appBar: AppBar(
+
+          title: Text("Heathens"),
+          backgroundColor: Colors.lightBlueAccent,
+          elevation: 0,
+          leading: Icon(Icons.menu),
+          actions: [
+            IconButton(onPressed: (){}, icon: Icon(Icons.logout)),
+          ],
+        ),
         body: Center(
           child: Container(
-              height: 300,
-              width: 300,
-              decoration: BoxDecoration(
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.circular(20),
+            height: 300,
+            width: 300,
+            decoration: BoxDecoration(
+              color: Colors.lightBlueAccent,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            padding: EdgeInsets.all(35),
+            child: Center(
+              child: Icon(
+                  Icons.favorite,
+                  color: Colors.white,
+                  //size: 32,
+                  size: 120,
               ),
-              padding: EdgeInsets.all(35),
-              child: Center(
-                child: Text(
-                  "NX D",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 45,
-                    fontWeight: FontWeight.bold,
-                ),
+/*             child: Text(
+                "NX D",
+                 style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 45,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
+*/
+
+            ),
           ),
         ),
       ),
@@ -39,4 +57,4 @@ class MyApp extends StatelessWidget{
   }
 }
 
-//https://youtu.be/HQ_ytw58tC4?t=2103 following this tutorial;
+//https://youtu.be/HQ_ytw58tC4?t=2223 following this tutorial;
